0f7994f8ed140cb84539e82ab3e70a5cbc88eefe
Tommy Suarez <159101789+tomtomcsg@users.noreply.github.com>
2024-03-10 22:51:15 -0400

    finished the basic template using JS for the tiles

--- wordle.js
+++ wordle.js
@@ -5,19 +5,23 @@ var row = 0;
 var col = 0; 
 
 var gameOver = false;
-var word = ["Tommy", "Haven", "squid" , "sloth"];
+var word = "Haven";
 
 window.onload = function(){
     initialize();
 }
 
 function initialize(){
-    for (let i = 0; i < height; i++){
-        for (let j = 0; j < width; j++){
-            
+
+    for (let r = 0; r < height; r++){
+        for (let c = 0; c < width; c++){
+            let tile = document.createElement("span");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.innerText = "";
+            document.getElementById("board").appendChild(tile);
         }
     }
-
 }
 
 
