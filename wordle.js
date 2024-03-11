87fea6405fe6c2d2e0a9f75ab33a735eb4467bd1
Tommy Suarez <159101789+tomtomcsg@users.noreply.github.com>
2024-03-10 21:53:33 -0400

    set up the board and starting functions

--- wordle.js
+++ wordle.js
@@ -4,6 +4,22 @@ var width = 5;
 var row = 0;
 var col = 0; 
 
+var gameOver = false;
+var word = ["Tommy", "Haven", "squid" , "sloth"];
+
+window.onload = function(){
+    initialize();
+}
+
+function initialize(){
+    for (let i = 0; i < height; i++){
+        for (let j = 0; j < width; j++){
+            
+        }
+    }
+
+}
+
 
 
 
