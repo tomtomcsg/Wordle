0040b4f465cbf33b0b856c16e9ca17404027ec6d
Tommy Suarez <159101789+tomtomcsg@users.noreply.github.com>
2024-03-12 12:56:56 -0400

    added the update function and its features

--- wordle.js
+++ wordle.js
@@ -5,7 +5,7 @@ var row = 0;
 var col = 0; 
 
 var gameOver = false;
-var word = "Haven";
+var word = "HAVEN";
 
 window.onload = function(){
     initialize();
@@ -22,8 +22,67 @@ function initialize(){
             document.getElementById("board").appendChild(tile);
         }
     }
+
+
+    document.addEventListener("keyup", (e) => {
+
+        if (gameOver) return
+
+        if ("KeyA" <= e.code && e.code <= "KeyZ"){
+            if (col < width){
+                let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+                if (currentTile.innerText == ""){
+                    currentTile.innerText = e.code[3];
+                    col += 1;
+                }
+            }
+        }
+        else if (e.code == "Backspace"){
+            if (0 < col && col <= width){
+                col -= 1; 
+            }
+            let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+            currentTile.innerText = "";
+        }
+        
+        else if (e.code == "Enter"){
+            update();
+            row += 1;
+            col = 0;
+        }
+
+        if (!gameOver && row == height){
+            gameOver == true;
+            document.getElementById("answer").innerText = word;
+
+        }
+
+    })
 }
 
+function update () {
+    let correct = 0;
+    for (let c = 0; c < width; c++){
+        let currentTile = document.getElementById(row.toString() + "-" + c.toString());
+        let letter = currentTile.innerText;
+
+        if (word[c]  == letter){
+            currentTile.classList.add("correct");
+            correct += 1;
+        }
+        else if (word.includes(letter)){
+            currentTile.classList.add("present");
+        }
+        else {
+            currentTile.classList.add("absent");
+        }
+
+        if (correct == width){
+            gameOver == true;
+        }
+    } 
+
+}
 
 
 
